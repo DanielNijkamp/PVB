d44f8d8e39eaeb8a87f4b1c7616f7bc3cc275033
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2024-04-04 13:29:14 +0200

    changed scene values and players are now tracked

--- Assets/Scripts/Player/CameraFocuser.cs
+++ Assets/Scripts/Player/CameraFocuser.cs
@@ -3,7 +3,6 @@ using Cinemachine;
 public class CameraFocuser : MonoBehaviour
 {
     [SerializeField] private CinemachineTargetGroup trackTargets;
-    private bool isFirstPlayer = true  ;
 
     private void Start()
     {
@@ -13,7 +12,6 @@ public class CameraFocuser : MonoBehaviour
     public void AddTargetToGroup()
     {
         Transform transform = gameObject.transform;
-        int groupId = isFirstPlayer ? 1 : 2;
-        trackTargets.AddMember(transform, groupId, 0);
+        trackTargets.AddMember(transform, 0, 0);
     }
 }
