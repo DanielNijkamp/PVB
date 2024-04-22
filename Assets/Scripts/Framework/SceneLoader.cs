b38118b256b14c15ac2e75de2f56fae914d4a7f0
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2024-04-22 11:18:07 +0200

    added game flow from begin menu to end

---
+++ Assets/Scripts/Framework/SceneLoader.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+  public static void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public static void AdditiveLoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Additive);
+    }
+    public static void LoadOnIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
