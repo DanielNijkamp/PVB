44f376d7138a47ad8f09484ad2fdd3f7f6e8743c
Daniel <danielnijkamp2004@gmail.com>
2024-04-25 15:57:47 +0200

    Level spawning

--- Assets/Scripts/Gameflow/LevelManager.cs
+++ Assets/Scripts/Gameflow/LevelManager.cs
@@ -1,22 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
+using Vector3 = UnityEngine.Vector3;
 
-public class LevelManager : MonoBehaviour
+public sealed class LevelManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> levels = new();
+    [SerializeField] private UnityEvent onCompletion = new();
+    [SerializeField] private Level[] levels = {};
+    private bool[] used;
     private int currentIndex;
-
-
-    public void DisplayNextPuzzle()
+    
+    private void Start()
     {
-        for (int i = currentIndex; i < levels.Count; i++)
+        used = new bool[levels.Length];
+        if (levels.Any())
         {
-            if(levels[i -2] != null)
-            {
-                levels[i - 2].SetActive(false);
-            }
-            //display next level
+            levels[0].gameObject.SetActive(true);
+            used[0] = true;
+            currentIndex = 1;
+            levels[0].gameObject.transform.position = Vector3.zero;
         }
     }
+
+    public void SpawnLevel()
+    {
+        var nonUsedIndices = Enumerable.Range(0, levels.Length).Where(i => !used[i]).ToList();
+
+        if (!nonUsedIndices.Any())
+        {
+            onCompletion?.Invoke();
+            return;
+        }
+
+        var index = nonUsedIndices[Random.Range(0, nonUsedIndices.Count)];
+        used[index] = true;
+
+        Level newLevel = levels[index];
+
+        if (currentIndex > 0)
+        {
+            Level currentLevel = levels[currentIndex - 1];
+            SetPosition(newLevel, currentLevel);
+        }
+
+        newLevel.gameObject.SetActive(true);
+        currentIndex++;
+    }
+
+    private void SetPosition(Level newLevel, Level currentLevel)
+    {
+        newLevel.transform.position = currentLevel.spawnPosition.position;
+    }
+    
+    public void DeSpawnOld()
+    {
+        if (currentIndex > 1)
+        {
+            levels[currentIndex - 2].gameObject.SetActive(false);
+        }
+        
+    }
+    
 }
