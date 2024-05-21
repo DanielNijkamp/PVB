b73ed06fc8c8eb9f4d5dc91a791821a7f50e1ccb
RossTubes <70896941+RossTubes@users.noreply.github.com>
2024-05-21 16:40:09 +0200

    updated Timer script
    I added a reset for the timer during the scene swtiching between the puzzles. So on puzzle completion when you walk through the door the timer gets reset.

--- Assets/Scripts/UI/UI Script/Timer.cs
+++ Assets/Scripts/UI/UI Script/Timer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Runtime.CompilerServices;
+using UnityEngine.Events;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float timeToComplete = 300f;
     private float initialTime;
+    [SerializeField] private UnityEvent sceneSwitcher = new UnityEvent();
     void Update()
     {
         if (timeToComplete > 0)
@@ -16,18 +18,28 @@ public class Timer : MonoBehaviour
             timeToComplete -= Time.deltaTime;
         }
 
-        if (timeToComplete <= 0)
+        if (timeToComplete <= 0 && timeToComplete != -1)
         {
             timeToComplete = 0;
             timerText.color = Color.red;
+            StartCoroutine(InvokeSceneSwitcherWithDelay(0.1f));
+            timeToComplete = -1;
         }
+
         int minutes = Mathf.FloorToInt(timeToComplete / 60);
         int seconds = Mathf.FloorToInt(timeToComplete % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
+    IEnumerator InvokeSceneSwitcherWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        sceneSwitcher?.Invoke();
+    }
+
     public void TimerReset()
     {
         timeToComplete = 300f;
+        timerText.color = Color.white;
     }
 }
