bc5ac5f5f16e00d6e54a5628e1799ec170fc371a
RossTubes <70896941+RossTubes@users.noreply.github.com>
2024-05-24 09:56:40 +0200

    added gravity and fixed interactble bug
    player could not interact with buttons anymore because the physics in the project settings were changed, now everything should work again.

--- Assets/Scripts/Player/Movement/Movement.cs
+++ Assets/Scripts/Player/Movement/Movement.cs
@@ -9,11 +9,11 @@ namespace Player
     [RequireComponent(typeof(CharacterController))]
     public sealed class Movement : MonoBehaviour
     {
-        [Header("Dependencies")] 
+        [Header("Dependencies")]
         [SerializeField] private Animator animator;
-        [SerializeField,AnimatorParam("animator")] private string animParam;
+        [SerializeField, AnimatorParam("animator")] private string animParam;
         private DirectionMediator directionMediator;
-        
+
         [Header("Settings")]
         [SerializeField] private float speed;
         [SerializeField] private float rotationSpeed;
@@ -49,26 +49,25 @@ namespace Player
 
             character.Move(velocity * Time.deltaTime);
         }
-
         private void FixedUpdate()
         {
             if (isFrozen) return;
-            
+
             Vector3 moveDirection = directionMediator.TransformInput(moveInput);
             var move = new Vector3(moveDirection.x, 0, moveDirection.y) * (speed * Time.deltaTime);
-            
+
             character.Move(move);
-            
+
             var isMoving = move != Vector3.zero;
             animator.SetBool(animParam, isMoving);
 
             if (!isMoving) return;
-            
+
             Quaternion targetRotation = Quaternion.LookRotation(move);
             Quaternion smoothedRotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             transform.rotation = smoothedRotation;
         }
-    
+
         public void Move(InputAction.CallbackContext context)
         {
             moveInput = context.ReadValue<Vector2>();
@@ -78,7 +77,6 @@ namespace Player
         {
             isFrozen = !isFrozen;
         }
-        
     }
 }
 
